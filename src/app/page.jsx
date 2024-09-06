c112981c5597f57f883229140a2ad6667323d6b3
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-09-06 20:07:36 +0500

    Enviornment setup and make the app responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 import styles from "./homepage.module.css";
-
+import Link from "next/link";
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/">Hello</Link>
+    <h1>Hey Blog App is running </h1>
+   
+    </div>;
 }
