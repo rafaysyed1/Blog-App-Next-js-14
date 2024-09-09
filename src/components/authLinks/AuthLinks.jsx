86fa50d69c2b0aa37b65502190e40448fdfec2d4
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-09-09 23:43:26 +0500

    Navbar progress

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "../authLinks/authLinks.module.css"
+
+const AuthLinks = ()=>{
+    return (
+        <div className={styles.container}>
+        AuthLinks
+        </div>
+    )
+}
+export default AuthLinks 
