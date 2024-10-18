63ca80c6afabe818fb25f7ba7c712f6d8e0b14cc
rafaysyed1 <smrafay2002@gmail.com>
2024-10-18 19:41:55 +0500

    rennovate the featured and category section

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,10 +1,75 @@
 import React from "react"
 import styles from "./categoryList.module.css"
+import Link from "next/link"
+import Image from "next/image"
 
-const CategoryList = ()=>{
+const CategoryList = () => {
     return (
         <div className={styles.container}>
-        CategoryList
+            <h1 className={styles.categoryTitle}>Popular Categories</h1>
+            <div className={styles.categories}>
+
+                <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+                    <Image
+                        src="/style.png"
+                        alt=""
+                        width={32}
+                        height={32}
+                        className={styles.categoryImage}
+                    />
+                    Style
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.fashion}`}>
+                    <Image
+                        src="/fashion.png"
+                        alt=""
+                        width={32}
+                        height={32}
+                        className={styles.categoryImage}
+                    />
+                    fashion
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.food}`}>
+                    <Image
+                        src="/food.png"
+                        alt=""
+                        width={32}
+                        height={32}
+                        className={styles.categoryImage}
+                    />
+                    food
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.travel}`}>
+                    <Image
+                        src="/travel.png"
+                        alt=""
+                        width={32}
+                        height={32}
+                        className={styles.categoryImage}
+                    />
+                    travel
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.culture}`}>
+                    <Image
+                        src="/culture.png"
+                        alt=""
+                        width={32}
+                        height={32}
+                        className={styles.categoryImage}
+                    />
+                    culture
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.coding}`}>
+                    <Image
+                        src="/coding.png"
+                        alt=""
+                        width={32}
+                        height={32}
+                        className={styles.categoryImage}
+                    />
+                    coding
+                </Link>
+            </div>
         </div>
     )
 }
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,27 @@
 import React from "react"
 import styles from "./featured.module.css"
+import Image from "next/image"
 
-const Featured = ()=>{
+const Featured = () => {
     return (
         <div className={styles.container}>
-        Featured
+            <h1 className={styles.title}>
+                <b>Hey, Rafay here!</b> Discover my humorous stories and creative ideas
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.postImage} />
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.postTitle}>Title Here</h1>
+                    <p className={styles.postDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Neque doloribus nemo officiis eligendi veritatis. 
+                        Voluptate iure error voluptatum dolor, beatae unde laboriosam facilis doloribus inventore
+                         quaerat quisquam voluptatibus quidem culpa.</p>
+                    <button className={styles.postActionButton}>Read More</button>
+                </div>
+
+            </div>
         </div>
     )
 }
