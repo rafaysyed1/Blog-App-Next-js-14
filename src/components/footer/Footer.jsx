48ea26e289be941d02f76d6028f26c046472ca93
rafaysyed1 <smrafay2002@gmail.com>
2024-10-21 22:10:15 +0500

    Added the category section and pagination now working on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react"
 import styles from "./footer.module.css"
+import Image from "next/image"
 
-const Footer = ()=>{
+const Footer = () => {
     return (
         <div className={styles.container}>
-        Footer
+            <div className={styles.info}>
+                <div className={styles.logo}>
+                    <Image src="/logo.png" alt="" width={50} height={50} />
+                    <h1 className={styles.logoText}>Rafay ka Blog</h1>
+                </div>
+            </div>
+            <div className={styles.links}>
+
+            </div>
         </div>
     )
 }
